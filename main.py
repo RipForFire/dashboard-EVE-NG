51405f2cbdbaf539729d8653e13245fef59f9c7d
RipForFire <wesleyvanderploeg99@gmail.com>
2023-03-19 21:13:59 +0000

    Allowed remote access

--- main.py
+++ main.py
@@ -59,4 +59,4 @@ def fix():
     return redirect(url_for('hello'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
--- setup.py
+++ setup.py
@@ -11,5 +11,4 @@ os.system("sudo apt-get install python3-pip -y")
 os.system("pip3 install -r requirements.txt")
 
 # Print message to tell the user that the installation is complete
-# And how to run the program
 print("Installation complete. To run the program, type 'python3 main.py'")
